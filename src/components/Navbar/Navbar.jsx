3f678755b7a91021542f8acd432271eb42a0c3e1
VikramKumarstm <vikramclg0000@gmail.com>
2025-10-23 20:06:04 +0530

    Fix: Added home page redirection from Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,18 @@
 // import React, { useContext } from "react";
 // import { CurrencyContext } from "../../context/CurrencyContext";
+import { useNavigate } from 'react-router-dom';
 import currencyStore from '../../state/store';
 
 function Navbar() {
 
   // const {setCurrency} = useContext(CurrencyContext);
   const {setCurrency} = currencyStore();
+
+  const navigate = useNavigate();
+
+  function goToHome() {
+    navigate('/')
+  }
 
   return (
     <div className="navbar bg-base-100 shadow-sm">
@@ -41,7 +48,7 @@ function Navbar() {
           </ul>
         </div>
       </div>
-      <div className="navbar-center">
+      <div onClick={goToHome} className="navbar-center">
         <a className="btn btn-ghost text-xl">Crypto Tracker</a>
       </div>
       <div className="navbar-end">
